4387a8201d9a995a51e124c27322c9e371d2710c
gurpreetsingh1300 <gurpreetsinghsethi13@gmail.com>
2019-05-09 02:24:01 +0530

    view categories

---
+++ QuickKart_EF/QuickKartConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickKartDataAccessLayer;
+
+
+namespace QuickKartConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateUser();
+            GetCatogeries();
+        }
+
+        public static void ValidateUser()
+        {
+            //Console.WriteLine("enter email");
+            string email = "Anzio_Don@infosys.com";//Console.ReadLine();
+
+            //Console.WriteLine("enter password");
+            string password = "don@123";// Console.ReadLine();
+
+            QuickKartRepository dal = new QuickKartRepository();
+            var role = dal.ValidateLoginUsingLinq(email,password);
+
+            Console.WriteLine(role);
+
+        }
+        public static void GetCatogeries()
+        {
+            QuickKartRepository dal = new QuickKartRepository();
+            var categories = dal.GetCategoriesUsingLinq();
+
+            Console.WriteLine(categories);
+        }
+
+
+    }
+}
--- QuickKart_EF/QuickKartDataAccessLayer/QuickKartRepository.cs
+++ QuickKart_EF/QuickKartDataAccessLayer/QuickKartRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace QuickKartDataAccessLayer
 {
     public class QuickKartRepository
@@ -16,9 +17,37 @@ namespace QuickKartDataAccessLayer
 
         public string ValidateLoginUsingLinq(string emailId, string password)
         {
+            string roleName = "";
             var objUser = (from usr in Context.Users
                            where usr.EmailId == emailId && usr.UserPassword == password
                            select usr).FirstOrDefault<User>();
+            if (objUser != null)
+            {
+                roleName = objUser.Role.RoleName;
+            }
+            else
+            {
+                roleName = "Invalid Credentials";
+            }
+            return roleName;
         }
+
+        public List<Category> GetCategoriesUsingLinq()
+        {
+            List<Category> lstCategories = null;
+            try
+            {
+                lstCategories = (from c in Context.Categories
+                                 orderby c.CategoryId
+                                 ascending
+                                 select c).ToList<Category>();
+            }
+            catch (Exception)
+            {
+                lstCategories = null;
+            }
+            return lstCategories;
+        }
+
     }
 }
