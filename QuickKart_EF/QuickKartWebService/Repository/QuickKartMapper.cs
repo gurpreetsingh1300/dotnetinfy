c0be0c10122f16f930ab542402e1fe6e7a87702f
gurpreetsingh1300 <gurpreetsinghsethi13@gmail.com>
2019-05-20 01:27:32 +0530

    trying for api

---
+++ QuickKart_EF/QuickKartWebService/Repository/QuickKartMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using QuickKartDataAccessLayer;
+
+namespace QuickKartWebService.Repository
+{
+    public class QuickKartMapper<Source, Destination> : Profile
+        where Source : class
+        where Destination : class
+    {
+        public QuickKartMapper()
+        {
+            CreateMap<Product, Models.Product>();
+            CreateMap<Models.Product, Product>();
+
+            //Mapper.Map<Product, Models.Product>(,);
+            ////From entity to model
+            //AutoMapper.Mapper.Map<Product, Models.Product>();
+            ////From model to entity
+            //Mapper.CreateMap<Models.Product, Product>();
+        }
+        public Destination Translate(Source obj)
+        {
+            return Mapper.Map<Destination>(obj);
+        }
+    }
+}
